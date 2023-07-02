65576707d76d0d30f395235de57e2925e1ef51e6
2017chase <1963482624@qq.com>
2023-07-03 03:30:10 +0800

    init

---
+++ google_search.py
@@ -0,0 +1,49 @@
+import requests
+import fire
+from urllib.parse import urlparse
+
+
+def get_domain(subdomain: str) -> list:
+    top = ['cn', 'org', 'com', 'edu']
+    tmp = subdomain.split('.')
+    ret = []
+    while tmp:
+        if tmp[0] not in top and tmp[1] in top:
+            ret.append('.'.join(tmp))
+        tmp.pop(0)
+    return ret
+
+
+def _google_search_internal(dork: str, start: int, num: int):
+    key = ""
+    cx = ""
+    proxy = {
+        'http': '127.0.0.1:7890'
+    }
+    r = requests.get(f"https://www.googleapis.com/customsearch/v1?key={key}&q={dork}&cx={cx}&start={start}&num={start}",
+                     proxies=proxy)
+    res = r.json()
+    ret = []
+    for item in res['items']:
+        url = urlparse(item['link'])
+        ret.append(f"[*] {item['title']} | {item['link']} | {url.netloc} | {' | '.join(get_domain(url.netloc))}")
+    return ret
+
+
+def googleSearch(input_file: str, output_file: str):
+    with open(input_file, "r", encoding="utf-8") as fp:
+        for dork in fp:
+            if dork.strip():
+                ret = _google_search_internal(dork.strip(), 0, 10)
+        # for t in ret:
+        #     print(t)
+        with open(output_file, "w", encoding="utf-8") as fp:
+            for line in ret:
+                fp.write(line + '\n')
+
+
+if __name__ == "__main__":
+    fire.Fire(googleSearch)
+    # print(GoogleSearch("百度", 10))
+    # print("test")
+    # test("site:edu.cn 深圳", 0, 10)
